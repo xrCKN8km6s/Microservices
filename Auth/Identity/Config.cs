95b7316b3f5d301455e6f99091d404c887643fef
xrCKN8km6s <1451773+xrCKN8km6s@users.noreply.github.com>
2019-03-26 17:57:07 +0200

    used identityserver template

---
+++ Auth/Identity/Config.cs
@@ -0,0 +1,104 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+
+using IdentityServer4.Models;
+using System.Collections.Generic;
+
+namespace Identity
+{
+    public static class Config
+    {
+        public static IEnumerable<IdentityResource> GetIdentityResources()
+        {
+            return new IdentityResource[]
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile(),
+                new IdentityResources.Email()
+            };
+        }
+
+        public static IEnumerable<ApiResource> GetApis()
+        {
+            return new[]
+            {
+                new ApiResource("orders", "Orders API")
+                {
+                    ApiSecrets = {new Secret("orders.secret".Sha256())}
+                }
+            };
+        }
+
+        public static IEnumerable<Client> GetClients()
+        {
+            return new[]
+            {
+                //// client credentials flow client
+                //new Client
+                //{
+                //    ClientId = "client",
+                //    ClientName = "Client Credentials Client",
+
+                //    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                //    ClientSecrets = { new Secret("511536EF-F270-4058-80CA-1C89C192F69A".Sha256()) },
+
+                //    AllowedScopes = { "api1" }
+                //},
+
+                // MVC client using hybrid flow
+                //new Client
+                //{
+                //    ClientId = "mvc",
+                //    ClientName = "MVC Client",
+
+                //    AllowedGrantTypes = GrantTypes.HybridAndClientCredentials,
+                //    ClientSecrets = { new Secret("49C1A7E1-0C79-4A89-A3D6-A37998FB86B0".Sha256()) },
+
+                //    RedirectUris = { "http://localhost:5001/signin-oidc" },
+                //    FrontChannelLogoutUri = "http://localhost:5001/signout-oidc",
+                //    PostLogoutRedirectUris = { "http://localhost:5001/signout-callback-oidc" },
+
+                //    AllowOfflineAccess = true,
+                //    AllowedScopes = { "openid", "profile", "email", "orders" }
+                //},
+
+                // SPA client using implicit flow
+                new Client
+                {
+                    ClientId = "spa",
+                    ClientName = "SPA Client",
+
+                    AllowedGrantTypes = GrantTypes.Implicit,
+                    AllowAccessTokensViaBrowser = true,
+                    AccessTokenType = AccessTokenType.Reference,
+
+                    RedirectUris =
+                    {
+                        "http://localhost:4200/signin-callback",
+                        "http://localhost:4200/silent-callback"
+                    },
+
+                    PostLogoutRedirectUris = {"http://localhost:4200"},
+                    AllowedCorsOrigins = {"http://localhost:4200"},
+                    RequireConsent = false,
+
+                    AllowedScopes = {"openid", "profile", "email", "orders"}
+                },
+
+                new Client
+                {
+                    ClientId = "ordersswaggerui",
+                    ClientName = "Orders Swagger UI",
+                    AllowedGrantTypes = GrantTypes.Implicit,
+                    AllowAccessTokensViaBrowser = true,
+                    RequireConsent = false,
+
+                    RedirectUris = {"https://localhost:5001/swagger/oauth2-redirect.html"},
+
+                    AllowedScopes = {"orders"}
+                }
+            };
+        }
+    }
+}
