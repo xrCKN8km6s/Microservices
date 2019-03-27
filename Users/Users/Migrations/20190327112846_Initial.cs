976be638d604a1fff1af31985f08bb67114c9327
xrCKN8km6s <1451773+xrCKN8km6s@users.noreply.github.com>
2019-03-27 14:50:17 +0200

    users initial

---
+++ Users/Users/Migrations/20190327112846_Initial.cs
@@ -0,0 +1,168 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace Users.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "permissions",
+                columns: table => new
+                {
+                    id = table.Column<long>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn),
+                    name = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_permissions", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "roles",
+                columns: table => new
+                {
+                    id = table.Column<long>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn),
+                    name = table.Column<string>(nullable: false),
+                    is_global = table.Column<bool>(nullable: false),
+                    is_active = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_roles", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "users",
+                columns: table => new
+                {
+                    id = table.Column<long>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn),
+                    sub = table.Column<string>(nullable: false),
+                    is_active = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_users", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "permission_roles",
+                columns: table => new
+                {
+                    permission_id = table.Column<long>(nullable: false),
+                    role_id = table.Column<long>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_permission_roles", x => new { x.role_id, x.permission_id });
+                    table.ForeignKey(
+                        name: "FK_permission_roles_permissions_permission_id",
+                        column: x => x.permission_id,
+                        principalTable: "permissions",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_permission_roles_roles_role_id",
+                        column: x => x.role_id,
+                        principalTable: "roles",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "user_roles",
+                columns: table => new
+                {
+                    user_id = table.Column<long>(nullable: false),
+                    role_id = table.Column<long>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_user_roles", x => new { x.role_id, x.user_id });
+                    table.ForeignKey(
+                        name: "FK_user_roles_roles_role_id",
+                        column: x => x.role_id,
+                        principalTable: "roles",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_user_roles_users_user_id",
+                        column: x => x.user_id,
+                        principalTable: "users",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_permission_roles_permission_id",
+                table: "permission_roles",
+                column: "permission_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_permission_roles_role_id_permission_id",
+                table: "permission_roles",
+                columns: new[] { "role_id", "permission_id" },
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_permissions_id",
+                table: "permissions",
+                column: "id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_roles_id",
+                table: "roles",
+                column: "id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_user_roles_user_id",
+                table: "user_roles",
+                column: "user_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_user_roles_role_id_user_id",
+                table: "user_roles",
+                columns: new[] { "role_id", "user_id" },
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_users_id",
+                table: "users",
+                column: "id");
+
+            migrationBuilder.InsertData("roles", 
+                new[] {"name", "is_global", "is_active"},
+                new object[] {"Admin", true, true});
+
+            migrationBuilder.InsertData("users",
+                new[] {"sub", "is_active"},
+                new object[] {"affc1ed6-e923-461f-8199-e95c07dc373b", true});
+
+            migrationBuilder.InsertData("user_roles",
+                new[] {"role_id", "user_id"},
+                new object[] {1, 1});
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "permission_roles");
+
+            migrationBuilder.DropTable(
+                name: "user_roles");
+
+            migrationBuilder.DropTable(
+                name: "permissions");
+
+            migrationBuilder.DropTable(
+                name: "roles");
+
+            migrationBuilder.DropTable(
+                name: "users");
+        }
+    }
+}
---
+++ Users/Users/Models/User.cs
@@ -0,0 +1,275 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Users.Models
+{
+    public class User
+    {
+        public long Id { get; set; }
+
+        public string Sub { get; set; }
+
+        public List<UserRole> UserRoles { get; set; }
+
+        public bool IsActive { get; set; }
+
+        public User()
+        {
+            UserRoles = new List<UserRole>();
+        }
+    }
+
+    public class UserRole
+    {
+        public long UserId { get; set; }
+
+        public User User { get; set; }
+
+        public long RoleId { get; set; }
+
+        public Role Role { get; set; }
+    }
+
+    public class Role
+    {
+        public long Id { get; set; }
+
+        public string Name { get; set; }
+
+        public bool IsGlobal { get; set; }
+
+        public bool IsActive { get; set; }
+
+        public List<UserRole> UserRoles { get; set; }
+
+        public List<PermissionRole> PermissionRoles { get; set; }
+
+        public Role()
+        {
+            UserRoles = new List<UserRole>();
+            PermissionRoles = new List<PermissionRole>();
+        }
+    }
+
+    public class PermissionRole
+    {
+        public long PermissionId { get; set; }
+
+        public Permission Permission { get; set; }
+
+        public long RoleId { get; set; }
+
+        public Role Role { get; set; }
+    }
+
+    public class Permission
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+
+        public List<PermissionRole> PermissionRoles { get; set; }
+
+        public Permission()
+        {
+            PermissionRoles = new List<PermissionRole>();
+        }
+    }
+
+    public class Permission1 : IEquatable<Permission1>
+    {
+        public long Id { get; private set; }
+
+        public string Description { get; private set; }
+
+        private  Permission1() { }
+
+        private Permission1(long id, string description)
+        {
+            Id = id;
+            Description = description;
+        }
+
+        public static Permission1 ViewOrders = new Permission1(1, "View orders");
+
+        public static Permission1 EditOrders = new Permission1(1, "Edit orders");
+
+        public bool Equals(Permission1 other)
+        {
+            if (ReferenceEquals(null, other)) return false;
+            if (ReferenceEquals(this, other)) return true;
+            return Id == other.Id;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != typeof(Permission1)) return false;
+            return Equals((Permission1) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return Id.GetHashCode();
+        }
+
+        public static bool operator ==(Permission1 left, Permission1 right)
+        {
+            return Equals(left, right);
+        }
+
+        public static bool operator !=(Permission1 left, Permission1 right)
+        {
+            return !Equals(left, right);
+        }
+
+        public static string NameFromId(long id)
+        {
+            return null;
+        }
+
+        public static long IdFromName(string name)
+        {
+            return 0;
+        }
+    }
+
+    public class UsersContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        public DbSet<Role> Roles { get; set; }
+
+        public DbSet<UserRole> UserRoles { get; set; }
+
+        public DbSet<Permission> Permissions { get; set; }
+
+        public UsersContext(DbContextOptions<UsersContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new UserEntityTypeConfiguration());
+            modelBuilder.ApplyConfiguration(new UserRoleEntityTypeConfiguration());
+            modelBuilder.ApplyConfiguration(new PermissionEntityTypeConfiguration());
+            modelBuilder.ApplyConfiguration(new PermissionRoleEntityTypeConfiguration());
+            modelBuilder.ApplyConfiguration(new RoleEntityTypeConfiguration());
+        }
+    }
+
+    public class UserEntityTypeConfiguration : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.ToTable("users");
+            builder.HasKey(p => p.Id);
+            builder.HasIndex(p => p.Id);
+            builder.Property(p => p.Id).HasColumnName("id").IsRequired();
+            builder.Property(p => p.Sub).HasColumnName("sub").IsRequired();
+            builder.Property(p => p.IsActive).HasColumnName("is_active").IsRequired();
+        }
+    }
+
+    public class UserRoleEntityTypeConfiguration : IEntityTypeConfiguration<UserRole>
+    {
+        public void Configure(EntityTypeBuilder<UserRole> builder)
+        {
+            builder.ToTable("user_roles");
+            builder.HasKey(k => new {k.RoleId, k.UserId});
+            builder.HasIndex(k => new {k.RoleId, k.UserId}).IsUnique();
+            builder.Property(p => p.RoleId).HasColumnName("role_id").IsRequired();
+            builder.Property(p => p.UserId).HasColumnName("user_id").IsRequired();
+            builder
+                .HasOne(m => m.User)
+                .WithMany(m => m.UserRoles)
+                .HasForeignKey(k => k.UserId);
+            builder
+                .HasOne(o => o.Role)
+                .WithMany(m => m.UserRoles)
+                .HasForeignKey(k => k.RoleId);
+        }
+    }
+
+    public class PermissionEntityTypeConfiguration : IEntityTypeConfiguration<Permission>
+    {
+        public void Configure(EntityTypeBuilder<Permission> builder)
+        {
+            builder.ToTable("permissions");
+            builder.HasKey(p => p.Id);
+            builder.HasIndex(p => p.Id);
+            builder.Property(p => p.Id).HasColumnName("id").IsRequired();
+            builder.Property(p => p.Name).HasColumnName("name").IsRequired();
+        }
+    }
+
+    public class PermissionRoleEntityTypeConfiguration : IEntityTypeConfiguration<PermissionRole>
+    {
+        public void Configure(EntityTypeBuilder<PermissionRole> builder)
+        {
+            builder.ToTable("permission_roles");
+            builder.HasKey(k => new {k.RoleId, k.PermissionId});
+            builder.HasIndex(k => new {k.RoleId, k.PermissionId}).IsUnique();
+            builder.Property(p => p.RoleId).HasColumnName("role_id").IsRequired();
+            builder.Property(p => p.PermissionId).HasColumnName("permission_id").IsRequired();
+            builder.HasOne(m => m.Permission)
+                .WithMany(m => m.PermissionRoles)
+                .HasForeignKey(k => k.PermissionId);
+            builder.HasOne(o => o.Role)
+                .WithMany(m => m.PermissionRoles)
+                .HasForeignKey(k => k.RoleId);
+        }
+    }
+
+    public class RoleEntityTypeConfiguration : IEntityTypeConfiguration<Role>
+    {
+        public void Configure(EntityTypeBuilder<Role> builder)
+        {
+            builder.ToTable("roles");
+            builder.HasKey(k => k.Id);
+            builder.HasIndex(p => p.Id);
+            builder.Property(p => p.Id).HasColumnName("id").IsRequired();
+            builder.Property(p => p.Name).HasColumnName("name").IsRequired();
+            builder.Property(p => p.IsGlobal).HasColumnName("is_global").IsRequired();
+            builder.Property(p => p.IsActive).HasColumnName("is_active").IsRequired();
+        }
+    }
+
+    internal class MicroserviceContextDesignFactory : IDesignTimeDbContextFactory<UsersContext>
+    {
+        public UsersContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder =
+                new DbContextOptionsBuilder<UsersContext>().UseNpgsql(
+                    "Host=localhost;Database=Users;Username=db_user;Password=db_pass");
+
+            return new UsersContext(optionsBuilder.Options);
+        }
+
+        private class DesignTimeMediator : IMediator
+        {
+            public Task<TResponse> Send<TResponse>(IRequest<TResponse> request,
+                CancellationToken cancellationToken = new CancellationToken())
+            {
+                return Task.FromResult<TResponse>(default);
+            }
+
+            public Task Publish(object notification, CancellationToken cancellationToken = new CancellationToken())
+            {
+                return Task.CompletedTask;
+            }
+
+            public Task Publish<TNotification>(TNotification notification,
+                CancellationToken cancellationToken = new CancellationToken()) where TNotification : INotification
+            {
+                return Task.CompletedTask;
+            }
+        }
+    }
+}
