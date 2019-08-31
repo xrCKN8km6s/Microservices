f2f38f8aa39b4885efaffc0d6862a6146d468475
xrCKN8km6s <1451773+xrCKN8km6s@users.noreply.github.com>
2019-09-01 01:43:20 +0300

    renaming refactoring

--- seed_db_dotnet.sh
+++ seed_db_dotnet.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -x
 
-dotnet ef database update -v --project IntegrationEventLog/IntegrationEventLog.csproj --startup-project Orders/Orders/Orders.csproj --context IntegrationEventLogContext
+dotnet ef database update -v --project IntegrationEventLog/IntegrationEventLog.csproj --startup-project Orders/Orders.API/Orders.API.csproj --context IntegrationEventLogContext
 
-dotnet ef database update -v --project Orders/Orders.Infrastructure/Orders.Infrastructure.csproj --startup-project Orders/Orders/Orders.csproj --context OrdersContext
+dotnet ef database update -v --project Orders/Orders.Infrastructure/Orders.Infrastructure.csproj --startup-project Orders/Orders.API/Orders.API.csproj --context OrdersContext
 
-dotnet ef database update -v --project Users/Users/Users.csproj
+dotnet ef database update -v --project Users/Users.API/Users.API.csproj
