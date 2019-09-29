05ff9849c92adf8b22814359a16847b00a78ca5e
xrCKN8km6s <1451773+xrCKN8km6s@users.noreply.github.com>
2019-09-29 18:04:03 +0300

    db scripts

--- seed_db_sql.sh
+++ seed_db_sql.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -x
 
+cat sql/0_init_db.sql | docker exec -i postgres psql -U db_user
+
 cat sql/Orders/*.sql sql/IntegrationEventLog/*.sql | docker exec -i postgres psql -d Orders -U db_user
 
 cat sql/Users/*.sql | docker exec -i postgres psql -d Users -U db_user
--- sql/0_init_db.sql
+++ sql/0_init_db.sql
@@ -1,3 +1,2 @@
-CREATE DATABASE "IntegrationEventLog";
 CREATE DATABASE "Orders";
 CREATE DATABASE "Users";
