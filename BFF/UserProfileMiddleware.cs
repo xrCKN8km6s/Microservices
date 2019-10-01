0915991b1ab364c42919744a2e44d13574ac84fe
xrCKN8km6s <1451773+xrCKN8km6s@users.noreply.github.com>
2019-10-01 22:33:22 +0300

    user profile middleware cache refactor

--- BFF/UserProfileMiddleware.cs
+++ BFF/UserProfileMiddleware.cs
@@ -2,57 +2,74 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Security.Claims;
+using System.Text.Json;
 using System.Threading.Tasks;
 using IdentityModel;
 using JetBrains.Annotations;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Caching.Distributed;
-using Newtonsoft.Json;
+using Microsoft.Extensions.Logging;
 using Users.Client.Contracts;
 
 namespace BFF
 {
-    //TODO: logging/error handling
     public class UserProfileMiddleware
     {
         private readonly RequestDelegate _next;
         private readonly IUsersClient _usersClient;
         private readonly IDistributedCache _cache;
+        private readonly ILogger<UserProfileMiddleware> _logger;
 
-        public UserProfileMiddleware(RequestDelegate next, IUsersClient usersClient, IDistributedCache cache)
+        public UserProfileMiddleware(RequestDelegate next, IUsersClient usersClient,
+            IDistributedCache cache, ILogger<UserProfileMiddleware> logger)
         {
-            _next = next;
-            _usersClient = usersClient;
-            _cache = cache;
+            _next = next ?? throw new ArgumentNullException(nameof(next));
+            _usersClient = usersClient ?? throw new ArgumentNullException(nameof(usersClient));
+            _cache = cache ?? throw new ArgumentNullException(nameof(cache));
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
         }
 
         [UsedImplicitly]
         public async Task InvokeAsync(HttpContext context)
         {
-            var subClaim = context.User.FindFirst(JwtClaimTypes.Subject);
+            var sub = context.User.FindFirstValue(JwtClaimTypes.Subject);
 
-            if (subClaim == null)
+            if (sub == null)
             {
+                _logger.LogError("sub claim is missing.");
                 context.Response.StatusCode = 401;
                 return;
             }
 
-            var profileCacheKey = $"profile_{subClaim.Value}";
+            var profileCacheKey = $"profile_{sub}";
 
-            var cached = await _cache.GetStringAsync(profileCacheKey);
+            var cached = await _cache.GetAsync(profileCacheKey);
 
             UserProfileDto profile;
 
-            if (string.IsNullOrWhiteSpace(cached))
+            if (cached == null)
             {
-                profile = await _usersClient.Profile_GetUserProfileAsync(subClaim.Value);
-                var content = JsonConvert.SerializeObject(profile);
-                await _cache.SetStringAsync(profileCacheKey, content,
-                    new DistributedCacheEntryOptions {AbsoluteExpirationRelativeToNow = TimeSpan.FromMinutes(5)});
+                profile = await _usersClient.Profile_GetUserProfileAsync(sub);
+
+                var exp = context.User.FindFirstValue(JwtClaimTypes.Expiration);
+
+                if (long.TryParse(exp, out var unixEpochSecs))
+                {
+                    var identityExp = DateTimeOffset.FromUnixTimeSeconds(unixEpochSecs);
+
+                    var offset = DateTimeOffset.UtcNow.AddMinutes(10);
+
+                    var absExp = offset > identityExp ? identityExp : offset;
+
+                    var content = JsonSerializer.SerializeToUtf8Bytes(profile);
+
+                    await _cache.SetAsync(profileCacheKey, content,
+                        new DistributedCacheEntryOptions { AbsoluteExpiration = absExp });
+                }
             }
             else
             {
-                profile = JsonConvert.DeserializeObject<UserProfileDto>(cached);
+                profile = JsonSerializer.Deserialize<UserProfileDto>(cached);
             }
 
             PrepareIdentity(context, profile);
