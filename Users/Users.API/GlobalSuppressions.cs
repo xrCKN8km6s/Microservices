78e389a9d1f962e44a6d318afd8dfb06640130a9
xrCKN8km6s <1451773+xrCKN8km6s@users.noreply.github.com>
2019-10-03 12:46:09 +0300

    suppressions

--- Users/Users.API/GlobalSuppressions.cs
+++ Users/Users.API/GlobalSuppressions.cs
@@ -5,3 +5,6 @@
 
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Reliability", "CA2007:Consider calling ConfigureAwait on the awaited task", Justification = "ASP.NET Core doesn't have synchronization context")]
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Performance", "CA1819:Properties should not return arrays", Justification = "It's safe to suppress for DTOs", Scope = "namespaceanddescendants", Target = "Users.API.DTO")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Performance", "CA1814:Prefer jagged arrays over multidimensional", Justification = "Migrations contract requirements", Scope = "namespaceanddescendants", Target = "Users.API.Migrations")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Design", "CA1062:Validate arguments of public methods", Justification = "Migrations are aute-generated", Scope = "namespaceanddescendants", Target = "Users.API.Migrations")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Usage", "CA2227:Collection properties should be read only", Justification = "EF models", Scope = "namespaceanddescendants", Target = "Users.API.Infrastructure")]
