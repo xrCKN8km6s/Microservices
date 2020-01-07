c9229d112dee412b6da9b79348a258a7f1bbe87d
xrCKN8km6s <1451773+xrCKN8km6s@users.noreply.github.com>
2020-01-07 17:29:12 +0200

    tokens & nuget updates

--- prepare_tls_tokens.ps1
+++ prepare_tls_tokens.ps1
@@ -1,27 +1,42 @@
 #!/usr/bin/env pwsh
 
 Param (
-    [Parameter(Position=0)][string[]] $cns = $(throw "-cn is required"),
+    [Parameter(Position=0)][string[]] $cns = $(throw "-cns is required"),
     [Parameter(Position=1)][string] $passwd = $(throw "-passwd is required"),
     [Parameter(Position=2)][string] $dir = 'certs'
 )
 
 openssl req -x509 -sha256 -newkey rsa:2048 -days 1095 `
-    -keyout "$dir/rootCA.key" -out "$dir/rootCA.crt" -passout "pass:$passwd"
+    -keyout "$dir/rootCA.key" -out "$dir/rootCA.crt" -passout "pass:$passwd" `
+    -subj "/C=UA/O=MicroservicesDEV/CN=rootCA"
 
 foreach ($cn in $cns) {
 
+"[req]
+prompt                      = no
+default_bits                = 2048
+distinguished_name          = req_distinguished_name
+req_extensions              = req_ext
+[req_distinguished_name]
+C=UA
+O=MicroservicesDEV
+CN=$cn
+[req_ext]
+subjectAltName              = @alt_names
+[alt_names]
+DNS.1                       = $cn" | Out-File "$dir/$cn.conf"
+
     openssl genrsa -out "$dir/$cn.key" 2048
 
-    openssl req -new -sha256 -key "$dir/$cn.key" -subj "/CN=$cn" -out "$dir/$cn.csr"
+    openssl req -new -sha256 -key "$dir/$cn.key" -out "$dir/$cn.csr" -config "$dir/$cn.conf"
 
     openssl x509 -req -in "$dir/$cn.csr" -CA "$dir/rootCA.crt" -CAkey "$dir/rootCA.key" `
-        -CAcreateserial -out "$dir/$cn.crt" -days 1095 -sha256 -passin "pass:$passwd"
+        -CAcreateserial -out "$dir/$cn.crt" -days 1095 -sha256 -passin "pass:$passwd" -extensions req_ext -extfile "$dir/$cn.conf"
 
     openssl pkcs12 -export -passin "pass:$passwd" -passout "pass:$passwd" `
         -out "$dir/$cn.pfx" `
         -inkey "$dir/$cn.key" `
         -in "$dir/$cn.crt";
 
-    Remove-Item "$dir/$cn.csr"
+    Remove-Item "$dir/$cn.csr", "$dir/$cn.conf"
 }
