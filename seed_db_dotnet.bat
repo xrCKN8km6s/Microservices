b387ec340da66f9e94b8553dce75c7d2af7d809d
xrCKN8km6s <1451773+xrCKN8km6s@users.noreply.github.com>
2020-05-08 19:34:34 +0300

    cleanup

--- seed_db_dotnet.bat
+++
@@ -1,5 +0,0 @@
-dotnet ef database update -v --project IntegrationEventLog/IntegrationEventLog.csproj --startup-project Orders/Orders.API/Orders.API.csproj --context IntegrationEventLogContext
-
-dotnet ef database update -v --project Orders/Orders.Infrastructure/Orders.Infrastructure.csproj --startup-project Orders/Orders.API/Orders.API.csproj --context OrdersContext
-
-dotnet ef database update -v --project Users/Users.API/Users.API.csproj
